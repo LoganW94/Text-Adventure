8076ab22e4f4b897b879a83d1bc49f77ca9bbede
LoganW94 <loganwogoman@gmail.com>
2017-01-19 00:23:21 -0500

    changed class self to player cause I felt like it

---
+++ player.py
@@ -0,0 +1,25 @@
+
+class Player:
+
+	__inventory = {"picture": 
+		"In the Picture there is a Boy and a Girl. They are sitting on a park bench on a sunny fall day",
+		"sword":
+		"A cheap sword. Probably a toy"
+		}
+
+	__credits = 0
+
+	__name = ""	
+
+	def __init__(self):
+		self.__name = "Default"
+
+	def printInventory(self):
+		for i in self.__inventory:
+			print(i)
+
+	def printCredits(self):
+		print("You have %d credits" % self.__credits)
+
+	def loadPlayer(self):
+		print("loading")			
